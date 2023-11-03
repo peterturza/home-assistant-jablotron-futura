5d845c64bee0af0011c9feb7433834d7b4992624
Peter Turza <peter.turza@ezmid.com>
2023-11-03 17:33:58 +0100

    updated api calls to version 2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## Version 0.1.1
+
+- updated api calls
+
 ## Version 0.1.0
 
 - fixed setting native values in number component
--- custom_components/jablotron_futura/const.py
+++ custom_components/jablotron_futura/const.py
@@ -2,11 +2,11 @@
 
 DOMAIN = "jablotron_futura"
 JABLOTRON = "Jablotron"
-JABLOTRON_API = "https://api.jablonet.net/api/1.9"
+JABLOTRON_API = "https://api.jablonet.net/api/2.2"
 JABLOTRON_BASE_URL = "https://api.jablonet.net"
 JABLOTRON_API_DEFAULT_HEADERS = {
     "x-vendor-id": "JABLOTRON:Jablotron",
-    "x-client-version": "MYJ-PUB-IOS-8.0.0.2176",
+    "x-client-version": "MYJ-PUB-IOS-8.3.2.3106",
     "Content-Type": "application/json",
     "Accept-Language": "en",
     "Accept": "application/json",
